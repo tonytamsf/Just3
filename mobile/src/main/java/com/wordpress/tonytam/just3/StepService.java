a4f7115275e7e19ab040199f77b0c828407b5d58
tonytam <tonytam@yahoo-inc.com>
2015-10-06 22:58:36 -0700

    tonytamsf/Just3#1 first version with dataItem sending, day 2 tasks

---
+++ mobile/src/main/java/com/wordpress/tonytam/just3/StepService.java
@@ -0,0 +1,31 @@
+package com.wordpress.tonytam.just3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class StepService extends WearableListenerService {
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        super.onDataChanged(dataEvents);
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(
+                        dataEvent.getDataItem())
+                        .getDataMap();
+                String path = dataEvent.getDataItem().getUri().getPath();
+                if (path.equals("/step-counter")) {
+                    int steps = dataMap.getInt("step-count");
+                    long time = dataMap.getLong("timstamp");
+                }
+            }
+        }
+    }
+}
--- wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
+++ wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
@@ -3,20 +3,30 @@ package com.wordpress.tonytam.just3;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class Just3Wear extends WearableActivity {
+public class Just3Wear extends WearableActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
 
     private BoxInsetLayout mContainerView;
     private TextView mTextView;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +35,15 @@ public class Just3Wear extends WearableActivity {
         setAmbientEnabled();
 
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
+        mTextView = (TextView) findViewById(R.id.title);
+
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+
     }
 
     @Override
@@ -58,4 +76,42 @@ public class Just3Wear extends WearableActivity {
             //mClockView.setVisibility(View.GONE);
         }
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public void sendStepCount(int steps, long timestamp) {
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/step-count");
+
+        putDataMapRequest.getDataMap().putInt("step-count", steps);
+        putDataMapRequest.getDataMap().putLong("timestamp", timestamp);
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                    @Override
+                    public void onResult(DataApi.DataItemResult dataItemResult) {
+                        if (!dataItemResult.getStatus().isSuccess()) {
+                            Log.d("debug","Fail to send step count" );
+
+                        } else {
+                            Log.d("debug","Successfully send step count" );
+                        }
+                        // https://www.udacity.com/course/viewer#!/c-ud875A/l-4582940110/m-4580800289
+                    }
+                });
+    }
+
 }
+
