55db9b869ab0ac35ecd7075a475a4372001e5dac
tonytam <tonytam@yahoo-inc.com>
2015-10-15 13:24:43 -0700

    - brute force save state

--- wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
+++ wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
@@ -6,17 +6,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
-import android.text.Editable;
-import android.text.method.KeyListener;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -31,7 +27,6 @@ import com.google.android.gms.wearable.Wearable;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -40,6 +35,7 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
+    private static int TAG_ITEM_NUM = 1;
     public static int LONG_PRESS_TIME = 1500; // Time in miliseconds
 
     private BoxInsetLayout mContainerView;
@@ -48,7 +44,7 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
     public int numLeft;
     HashMap<Integer, Integer> colorMapOn;
     HashMap<Integer, Integer> colorMapOff;
-    HashMap<Integer, Integer> itemState;
+    HashMap<Integer, Boolean> itemDoneState;
     private float origTextSize;
     private TextView longPressedView;
     final Handler _handler = new Handler();
@@ -88,6 +84,7 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
         colorMapOff.put(R.id.item2, R.color.item2_off);
         colorMapOff.put(R.id.item3, R.color.item3_off);
 
+        itemDoneState = new HashMap<Integer, Boolean>(3);
         refreshViewWithData();
 
         // Map color
@@ -118,6 +115,7 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
 
         for (int i : ids) {
             TextView v = (TextView) findViewById(i);
+            v.setTag(i + 1);
             v.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
@@ -197,10 +195,14 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
         Log.d("OnClick", textView.getText().toString());
 
         int c = textView.getCurrentTextColor();
+        Log.d("onClick : set Item", textView.getTag().toString());
+
         if (c != Color.GRAY) {
             setItemDone(textView);
+            itemDoneState.put((Integer) textView.getTag(), true);
 
         } else {
+            itemDoneState.put((Integer) textView.getTag(), false);
             setItemNew(textView);
         }
         updateDisplay();
@@ -290,23 +292,46 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
     public void refreshViewWithData() {
         // Load Data
         ArrayList<String> data = loadDataFromPreferences();
+
         ((TextView) findViewById(R.id.item1)).setText(data.get(0));
         ((TextView) findViewById(R.id.item2)).setText(data.get(1));
         ((TextView) findViewById(R.id.item3)).setText(data.get(2));
+        int ids[] = {R.id.item1, R.id.item2, R.id.item3};
+
+        for (int i = 1; i <= 3; i++) {
+            if(itemDoneState.get(findViewById(ids[i-1]).getTag())) {
+                Log.d("setItemDone", findViewById(ids[i-1]).getTag().toString());
+                setItemDone((TextView) findViewById(ids[i - 1]));
+            } else {
+                Log.d("setItemNew", findViewById(ids[i-1]).getTag().toString());
+                setItemNew((TextView) findViewById(ids[i - 1]));
+            }
+        }
     }
 
     public ArrayList<String> loadDataFromPreferences() {
         ArrayList<String> result = new ArrayList<String>(3);
+        int ids[] = {R.id.item1, R.id.item2, R.id.item3};
 
         Context context = getApplicationContext();
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         result.add(sharedPref.getString("item1", ""));
         result.add(sharedPref.getString("item2", ""));
         result.add(sharedPref.getString("item3", ""));
+        itemDoneState.put((int) findViewById(ids[0]).getTag(), sharedPref.getString("item1State", "false") == "true");
+        itemDoneState.put((int) findViewById(ids[1]).getTag(), sharedPref.getString("item2State", "false") == "true");
+        itemDoneState.put((int) findViewById(ids[2]).getTag(), sharedPref.getString("item3State", "false") == "true");
+
+        Log.d("lP:itemState1", sharedPref.getString("item1State", "false"));
+        Log.d("lP:itemState2", sharedPref.getString("item2State", "false"));
+        Log.d("lP:itemState3", sharedPref.getString("item3State", "false"));
+
         return result;
     }
 
     public void sendTriData() {
+        int ids[] = {R.id.item1, R.id.item2, R.id.item3};
+
         PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/v1/tri-data");
         putDataMapRequest.getDataMap().putString("item1", ((TextView) findViewById(R.id.item1)).getText().toString());
         putDataMapRequest.getDataMap().putString("item2", ((TextView) findViewById(R.id.item2)).getText().toString());
@@ -333,6 +358,15 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
         editor.putString("item1", ((TextView) findViewById(R.id.item1)).getText().toString());
         editor.putString("item2", ((TextView) findViewById(R.id.item2)).getText().toString());
         editor.putString("item3", ((TextView) findViewById(R.id.item3)).getText().toString());
+
+        editor.putString("item1State", itemDoneState.get((int) findViewById(ids[0]).getTag()).toString());
+        editor.putString("item2State", itemDoneState.get((int) findViewById(ids[1]).getTag()).toString());
+        editor.putString("item3State", itemDoneState.get((int) findViewById(ids[2]).getTag()).toString());
+
+        Log.d("save item1State", itemDoneState.get((int) findViewById(ids[0]).getTag()).toString());
+        Log.d("save item2State", itemDoneState.get((int) findViewById(ids[1]).getTag()).toString());
+        Log.d("save item3State", itemDoneState.get((int) findViewById(ids[2]).getTag()).toString());
+
         editor.commit();
     }
 
