b49cf09e79439e02a2c5eac0b15f64da89dd352a
tonytam <tonytam@yahoo-inc.com>
2015-11-09 10:06:07 -0800

    - version 1.4 : better voice

--- wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
+++ wear/src/main/java/com/wordpress/tonytam/just3/Just3Wear.java
@@ -449,10 +449,17 @@ public class Just3Wear extends WearableActivity implements GoogleApiClient.Conne
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,
+                true);
+        intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE,
+                true);
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,
                 1);
         intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE,
                 1);
+        intent.putExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES,
+                0.5
+                );
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
                 true);
         // Start the activity, the intent will be populated with the speech text
